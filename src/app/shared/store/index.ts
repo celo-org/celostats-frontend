c8f72d50fcbc8c0fdc08bed571aba6f869ea6b19
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-01-31 14:22:07 +0100

    Expose Nodes (sorting) state

--- src/app/shared/store/index.ts
+++ src/app/shared/store/index.ts
@@ -10,26 +10,36 @@ import { Effect } from '@ngrx/effects'
 import { environment } from 'src/environments/environment'
 
 import * as Ethstats from './ethstats'
+import * as NodesSorting from './nodes-sorting'
 
 export const effects: any[] = [
   Ethstats.Effects,
+  NodesSorting.Effects,
 ]
 
 export interface AppState {
   ethstats: Ethstats.State
+  nodesSorting: NodesSorting.State,
 }
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : []
 
 export const reducers: ActionReducerMap<AppState> = {
   ethstats: Ethstats.reducer,
+  nodesSorting: NodesSorting.reducer,
 }
 
 // Root selectors
 export const selectEthstats = (state: AppState) => state.ethstats
+export const selectNodesSorting = (state: AppState) => state.nodesSorting
 
 // Ethstats Selectors
 export const getEthstatsNodes = createSelector(selectEthstats, Ethstats.getNodes)
 export const getEthstatsNodesList = createSelector(selectEthstats, Ethstats.getNodesList)
 export const getEthstatsLastBlock = createSelector(selectEthstats, Ethstats.getLastBlock)
 export const getEthstatsCharts = createSelector(selectEthstats, Ethstats.getChars)
+
+// Nodes (sorting)
+export const getNodesSortingColumns = createSelector(selectNodesSorting, NodesSorting.getColumns)
+export const getNodesSortingSorting = createSelector(selectNodesSorting, NodesSorting.getSorting)
+
--- src/app/shared/store/nodes-sorting/index.ts
+++ src/app/shared/store/nodes-sorting/index.ts
@@ -1,5 +1,6 @@
 export * from './nodes-sorting.state'
 export * from './nodes-sorting.reducers'
+export { NodesSortingEffects as Effects } from './nodes-sorting.effects'
 
 import * as _actions from './nodes-sorting.actions'
 export const actions = _actions
