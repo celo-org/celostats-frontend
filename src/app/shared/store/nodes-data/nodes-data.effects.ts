2ac24030c57bde79751d9006f6b9693a876c3f3f
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-02-05 16:04:20 +0100

    Stop refreshing nodes rows data when the app is in background

--- src/app/shared/store/nodes-data/nodes-data.effects.ts
+++ src/app/shared/store/nodes-data/nodes-data.effects.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { Store, select } from '@ngrx/store';
+import { Store, select, Action } from '@ngrx/store';
 import { Actions, createEffect, ofType, ROOT_EFFECTS_INIT } from '@ngrx/effects'
 import { interval } from 'rxjs'
 import { mergeMap, filter, first, pairwise, startWith, tap, map, combineLatest, distinctUntilChanged } from 'rxjs/operators'
@@ -15,64 +15,68 @@ import { AppState } from './nodes-data.state'
 @Injectable()
 export class NodesDataEffects {
 
-  generateNodeRowFromUpdates$ = createEffect(() => this.actions$.pipe(
-    ofType(ethstatsActions.updateNodes),
-    mergeMap(() =>
-      this.store.pipe(
-        select(fromEthstats.select),
-        select(fromEthstats.getNodes),
-        first(),
-      )
-    ),
-    startWith({} as fromEthstats.State['nodes']),
-    combineLatest(interval(1000)),
-    pairwise(),
-    map(([[previous, pInt], [current, cInt]]) => {
-      const force = pInt !== cInt
-      const updated: typeof current[''][] = []
-      Object.keys(current)
-        .forEach(id => {
-          if (force || current[id].updates !== previous[id]?.updates) {
-            updated.push(current[id])
-          }
-        })
-      return updated
-    }),
-    combineLatest(
-      this.store.pipe(
-        select(fromNodesSorting.select),
-        select(fromNodesSorting.getColumns),
+  generateNodeRowFromUpdates$ = createEffect(() => this.actions$
+    .pipe(
+      ofType(ethstatsActions.updateNodes),
+      mergeMap(() =>
+        this.store.pipe(
+          select(fromEthstats.select),
+          select(fromEthstats.getNodes),
+          first(),
+        )
       ),
-      this.store.pipe(
-        select(fromEthstats.select),
-        select(fromEthstats.getLastBlock),
-        map(block => block?.number),
-        distinctUntilChanged(),
+      startWith({} as fromEthstats.State['nodes']),
+      combineLatest(interval(1000)),
+      filter(() => document.hidden === undefined ? true : !document.hidden),
+      pairwise(),
+    )
+    .pipe(
+      map(([[previous, pInt], [current, cInt]]) => {
+        const force = pInt !== cInt
+        const updated: typeof current[''][] = []
+        Object.keys(current)
+          .forEach(id => {
+            if (force || current[id].updates !== previous[id]?.updates) {
+              updated.push(current[id])
+            }
+          })
+        return updated
+      }),
+      combineLatest(
+        this.store.pipe(
+          select(fromNodesSorting.select),
+          select(fromNodesSorting.getColumns),
+        ),
+        this.store.pipe(
+          select(fromEthstats.select),
+          select(fromEthstats.getLastBlock),
+          map(block => block?.number),
+          distinctUntilChanged(),
+        ),
       ),
-    ),
-    map(([nodes, columns, block]) => {
-      const time = Date.now()
-      return nodes
-        .filter(({id}) => !!id)
-        .map(node => ({
-          id: node.id,
-          columns: columns
-            .map(column => {
-              const context = {block, node, time}
-              const value = column.accessor(node, context)
-              return {
-                raw: value,
-                type: column.type,
-                value: column.show(value, context),
-                style: column.color(value, context),
-                link: column.link(value, context),
-                variants: column.variants,
-              }
-            }),
-        }))
-    }),
-    map(rows => nodesDataActions.updateRawData({rows})),
-  ))
+      map(([nodes, columns, block]) => {
+        const time = Date.now()
+        return nodes
+          .filter(({id}) => !!id)
+          .map(node => ({
+            id: node.id,
+            columns: columns
+              .map(column => {
+                const context = {block, node, time}
+                const value = column.accessor(node, context)
+                return {
+                  raw: value,
+                  type: column.type,
+                  value: column.show(value, context),
+                  style: column.color(value, context),
+                  link: column.link(value, context),
+                  variants: column.variants,
+                }
+              }),
+          }))
+      }),
+      map(rows => nodesDataActions.updateRawData({rows})),
+    ))
 
   cleanRowsData$ = createEffect(() => this.actions$.pipe(
     ofType(nodesDataActions.updateRawData, nodesSortingActions.orderBy),
