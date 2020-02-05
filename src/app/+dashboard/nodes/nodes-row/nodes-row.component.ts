baec5f38443acd31a65326cb5877f8341d69aabb
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-02-05 17:06:39 +0100

    Improve performance of Nodes row

--- src/app/+dashboard/nodes/nodes-row/nodes-row.component.ts
+++ src/app/+dashboard/nodes/nodes-row/nodes-row.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { Store, select } from '@ngrx/store'
 import { Observable } from 'rxjs'
-import { pluck } from 'rxjs/operators'
+import { pluck, map } from 'rxjs/operators'
 
 import { AppState, getNodesDataDataOf } from 'src/app/shared/store'
 import { DataRow } from 'src/app/shared/store/nodes-data'
@@ -9,12 +9,13 @@ import { DataRow } from 'src/app/shared/store/nodes-data'
 @Component({
   selector: 'app-dashboard-nodes-row',
   templateUrl: './nodes-row.component.html',
-  styleUrls: ['./nodes-row.component.scss']
+  styleUrls: ['./nodes-row.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardNodesRowComponent implements OnInit {
   @Input() rowId: string
 
-  columns$: Observable<DataRow['columns']>
+  columns$: Observable<DataRow['columns'] & {classNames: string}[]>
 
 
   constructor(private store: Store<AppState>) { }
@@ -23,6 +24,18 @@ export class DashboardNodesRowComponent implements OnInit {
     this.columns$ = this.store.pipe(
       select(getNodesDataDataOf, {rowId: this.rowId}),
       pluck('columns'),
+      map(columns =>
+        columns
+          .map(column => ({
+            ...column,
+            classNames: `
+              table__cell
+              table__cell--${column.style}
+              table__cell--type-${column.type}
+              ${this.variants(column.variants)}
+            `.replace(/\s+/g, ' ').trim(),
+          }))
+      )
     )
   }
 
