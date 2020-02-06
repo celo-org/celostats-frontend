18e0d0b70149a24c9906b6b04428a45f90404adc
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-02-06 01:38:05 +0100

    Implement a custom strategy to detach some rows from the change detector

--- src/app/+dashboard/nodes/nodes-row/nodes-row.component.ts
+++ src/app/+dashboard/nodes/nodes-row/nodes-row.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Store, select } from '@ngrx/store'
-import { Observable } from 'rxjs'
-import { pluck, map } from 'rxjs/operators'
+import { Observable, Subscription } from 'rxjs'
+import { pluck, map, share, pairwise, filter, startWith, scan, tap } from 'rxjs/operators'
 
 import { AppState, getNodesDataDataOf } from 'src/app/shared/store'
 import { DataRow } from 'src/app/shared/store/nodes-data'
+import { Column } from 'src/app/shared/store/nodes-sorting'
 
 @Component({
   selector: 'app-dashboard-nodes-row',
@@ -12,17 +13,22 @@ import { DataRow } from 'src/app/shared/store/nodes-data'
   styleUrls: ['./nodes-row.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashboardNodesRowComponent implements OnInit {
+export class DashboardNodesRowComponent implements OnInit, OnDestroy {
   @Input() rowId: string
 
   columns$: Observable<DataRow['columns'] & {classNames: string}[]>
+  private row$: Observable<DataRow>
+  private changeDetectionsSubscription: Subscription
+  private readonly detachAfter = 6
 
-
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
-    this.columns$ = this.store.pipe(
+    this.row$ = this.store.pipe(
       select(getNodesDataDataOf, {rowId: this.rowId}),
+      share(),
+    )
+    this.columns$ = this.row$.pipe(
       pluck('columns'),
       map(columns =>
         columns
@@ -37,6 +43,38 @@ export class DashboardNodesRowComponent implements OnInit {
           }))
       )
     )
+
+    this.checkChanges()
+  }
+
+  ngOnDestroy() {
+    this.changeDetectionsSubscription.unsubscribe()
+  }
+
+  checkChanges() {
+    this.changeDetectionsSubscription = this.row$.pipe(
+      map(({columns}) =>
+        columns
+          .map(({value, style}) => [value, style])
+          .flat()
+          .join('|')
+      ),
+      startWith(''),
+      pairwise(),
+      scan((updates, [a, b]) => a !== b ? 0 : updates + 1, 0),
+    )
+      .subscribe(updates => {
+        // Internal data to know if the change detector is attached
+        const isAttached = !!((this.cdr as any)._lView[2] & 128)
+
+        if (!isAttached && !updates) {
+          this.cdr.detectChanges()
+          this.cdr.reattach()
+        }
+        if (isAttached && updates >= this.detachAfter) {
+          this.cdr.detach()
+        }
+      })
   }
 
   variants(variants: Column['variants']) {
