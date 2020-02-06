0529b3bda65a4f57b761471f88b8793e89331cee
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-02-06 17:38:16 +0100

    Export tests results to CircleCI

--- karma.conf.js
+++ karma.conf.js
@@ -10,17 +10,23 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
+    reporters: ['progress', 'kjhtml', 'junit'],
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ethstats-frontend'),
+      dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputDir: require('path').join(__dirname, './coverage/junit'),
+      outputFile: 'test-results.xml',
+      useBrowserName: false
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
