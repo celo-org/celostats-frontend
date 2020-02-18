391ec2948104b2a5a4b2468bdaf4fca6673c2f28
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-02-18 14:20:46 +0100

    Improve visualization of MicroChart with no data

--- src/app/components/micro-chart/micro-chart.component.ts
+++ src/app/components/micro-chart/micro-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, ViewChild, OnInit, OnChanges, OnDestroy, SimpleChanges, ChangeDetectionStrategy, ElementRef, ChangeDetectorRef } from '@angular/core'
-import { Subject, Subscription } from 'rxjs'
-import { throttleTime, shareReplay, tap } from 'rxjs/operators'
+import { Subject, Subscription, animationFrameScheduler, interval } from 'rxjs'
+import { throttleTime, shareReplay, throttle, tap } from 'rxjs/operators'
 
 import { colorRange } from 'src/app/shared'
 
@@ -9,6 +9,7 @@ enum BarColor {
   warn1 = '#edfa5a',
   warn2 = '#ffa174',
   warn3 = '#ff7088',
+  no = '#cccccc',
 }
 
 @Component({
@@ -25,10 +26,12 @@ export class MicroChartComponent implements OnInit, OnChanges, OnDestroy {
   private onDataSubscription: Subscription
   private cleanData: {value: number, color: string}[]
   private ctx: CanvasRenderingContext2D
+  private noColor: boolean
   private readonly height = 18
   private readonly width = 120
   private readonly barSize = 2
   private readonly barSpace = 1
+  private readonly barNum = 40
 
   constructor(private cdr: ChangeDetectorRef) {
     cdr.detach()
@@ -41,7 +44,11 @@ export class MicroChartComponent implements OnInit, OnChanges, OnDestroy {
 
     this.ctx = canvas.getContext('2d')
 
-    this.onData$.pipe(throttleTime(100))
+    this.onData$
+      .pipe(
+        throttleTime(100),
+        throttle(() => interval(0, animationFrameScheduler)),
+      )
       .subscribe(() => this.generateChart())
 
     this.onData$.next()
@@ -69,13 +76,23 @@ export class MicroChartComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   generateCleanData() {
-    const max = this.data.reduce((acc, _) => Math.max(acc, _), 0)
+    const data = [...this.data || []]
+    const max = data.reduce((acc, _) => Math.max(acc, _), 0)
 
-    this.cleanData = this.data
+    this.cleanData = data
+      .slice(0, 40)
       .map(_ => ({
         value: Math.max(_ / max, 0),
         color: this.getColor(_),
       }))
+
+    if (this.cleanData.length < this.barNum) {
+      this.cleanData = [
+        ...this.cleanData,
+        ...new Array(this.barNum - this.cleanData.length)
+          .fill({value: 0, color: BarColor.no}),
+      ] as any[]
+    }
   }
 
   getColor(n: number) {
