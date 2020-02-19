8cb21b7382e6d79a61e853b6c9131976a7baed7f
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-02-19 13:43:13 +0100

    Create a tooltip for MicroChart

--- src/app/components/micro-chart/micro-chart.component.ts
+++ src/app/components/micro-chart/micro-chart.component.ts
@@ -1,10 +1,30 @@
 import { Component, Input, ViewChild, OnInit, OnChanges, OnDestroy, SimpleChanges, ChangeDetectionStrategy, ElementRef, ChangeDetectorRef } from '@angular/core'
-import { Subject, Subscription, animationFrameScheduler, interval } from 'rxjs'
-import { throttleTime, shareReplay, throttle, tap } from 'rxjs/operators'
+import { Subject, Subscription, animationFrameScheduler, interval, fromEvent } from 'rxjs'
+import { debounceTime, shareReplay, throttle, map, tap, combineLatest, mergeMap, takeUntil, startWith, endWith, distinctUntilChanged } from 'rxjs/operators'
 
 import { colorRange } from 'src/app/shared'
 
+// Seen on: https://stackoverflow.com/a/7838871
+declare global {
+  interface CanvasRenderingContext2D {
+    roundRect: (this: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, r: number) => CanvasRenderingContext2D
+  }
+}
+CanvasRenderingContext2D.prototype.roundRect = function (x, y, w, h, r) {
+  if (w < 2 * r) r = w / 2
+  if (h < 2 * r) r = h / 2
+  this.beginPath()
+  this.moveTo(x+r, y)
+  this.arcTo(x+w, y,   x+w, y+h, r)
+  this.arcTo(x+w, y+h, x,   y+h, r)
+  this.arcTo(x,   y+h, x,   y,   r)
+  this.arcTo(x,   y,   x+w, y,   r)
+  this.closePath()
+  return this
+}
+
 enum BarColor {
+  info = '#73ddff',
   ok = '#35d07f',
   warn1 = '#edfa5a',
   warn2 = '#ffa174',
@@ -20,19 +40,21 @@ enum BarColor {
 })
 export class MicroChartComponent implements OnInit, OnChanges, OnDestroy {
   @Input() data: number[]
-  @Input() show: (n :number) => string = _ => String(_)
+  @Input() show: (n: number) => string
   @ViewChild('canvas', {static: true}) canvas: ElementRef
 
   private onData$ = new Subject()
   private onDataSubscription: Subscription
-  private cleanData: {value: number, color: string}[]
+  private cleanData: {value: number, color: string, label: string}[]
   private ctx: CanvasRenderingContext2D
   private noColor: boolean
   private readonly height = 18
   private readonly width = 120
+  private readonly barMinHeight = 2
   private readonly barSize = 2
   private readonly barSpace = 1
   private readonly barNum = 40
+  private readonly labelPadding = 2
 
   constructor(private cdr: ChangeDetectorRef) {
     cdr.detach()
@@ -47,10 +69,26 @@ export class MicroChartComponent implements OnInit, OnChanges, OnDestroy {
 
     this.onData$
       .pipe(
-        throttleTime(100),
+        debounceTime(100),
         throttle(() => interval(0, animationFrameScheduler)),
+        combineLatest(
+          fromEvent(canvas, 'mouseenter')
+            .pipe(
+              mergeMap(() =>
+                fromEvent(canvas, 'mousemove')
+                  .pipe(
+                    map(({offsetX}) => Math.floor((offsetX - this.barSize) / (this.barSize + this.barSpace))),
+                    map(bar => Math.max(0, Math.min(this.barNum - 1, bar))),
+                    takeUntil(fromEvent(canvas, 'mouseleave')),
+                    endWith(false),
+                  ),
+              ),
+              startWith(false),
+              distinctUntilChanged(),
+            ),
+        ),
       )
-      .subscribe(() => this.generateChart())
+      .subscribe(([, x]) => this.generateChart(x))
 
     this.onData$.next()
   }
@@ -68,12 +106,12 @@ export class MicroChartComponent implements OnInit, OnChanges, OnDestroy {
     }
   }
 
-  generateChart() {
+  generateChart(hoverBar: number | boolean) {
     if (!this.data || !this.ctx) {
       return
     }
     this.generateCleanData()
-    this.render()
+    this.render(hoverBar)
   }
 
   generateCleanData() {
@@ -85,7 +123,7 @@ export class MicroChartComponent implements OnInit, OnChanges, OnDestroy {
       .map(_ => ({
         value: Math.max(_ / max, 0),
         color: this.getColor(_),
-        label: this.show(_),
+        label: this?.show(_) || String(_),
       }))
 
     if (this.cleanData.length < this.barNum) {
@@ -98,22 +136,52 @@ export class MicroChartComponent implements OnInit, OnChanges, OnDestroy {
   }
 
   getColor(n: number) {
-    const color = colorRange(n, [, 100, 500, 2000])
+    const color = colorRange(n, [1, 100, 500, 2000])
     return BarColor[color]
   }
 
-  render() {
+  render(hoverBar: number | boolean) {
     const step = this.barSize + this.barSpace
     const ctx = this.ctx
 
-    ctx.clearRect(0, 0, 1000, 1000)
-
+    ctx.clearRect(0, 0, this.width, this.height)
     this.cleanData
       .forEach((({value, color}, i) => {
-        const h = Math.max((this.height * value), 1)
+        if (hoverBar !== false) {
+          ctx.globalAlpha = i === hoverBar ? 1 : 0.5
+        }
+        const h = Math.max(((this.height - this.barMinHeight) * value), 0) + this.barMinHeight
 
         ctx.fillStyle = color
         ctx.fillRect(step * i, this.height - h, this.barSize, h)
       }))
+    ctx.globalAlpha = 1
+
+    if (typeof hoverBar === 'number') {
+      const isLeft = hoverBar < (this.barNum / 2)
+      const bar = this.cleanData[hoverBar] || {label: 'n/a'}
+
+      ctx.font = `10px "Roboto Mono"`
+
+      const {width} = ctx.measureText(bar.label)
+      const padding = (this.height / 2) - this.labelPadding
+      const startFrom = !isLeft
+        ? this.labelPadding
+        : this.width - this.barSpace - (width + (padding * 2)) - this.labelPadding
+
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.6)'
+      ctx.roundRect(
+        startFrom,
+        this.labelPadding,
+        width + (padding * 2),
+        (this.height - this.labelPadding * 2),
+        padding,
+      )
+      ctx.fill()
+
+      ctx.fillStyle = '#000000'
+      ctx.textBaseline = 'middle'
+      ctx.fillText(bar.label, startFrom + padding, this.height / 2 + 1)
+    }
   }
 }
--- src/app/shared/store/nodes-sorting/columns.data.ts
+++ src/app/shared/store/nodes-sorting/columns.data.ts
@@ -129,7 +129,7 @@ export const columns: Column[] = [
     icon: 'wifi_tethering',
     variants: ['medium'],
     accessor: node => node.block?.propagation,
-    show: (value, {node}) => value ? `${value} ms` : null,
+    show: (value, {node}) => value ? timeAgo(value as any, true, 10000) : null,
     color: (value, {node}) => value === null ? 'no' : colorRange(+value, [10, 100, 1000, 10000, 100000]),
   },
   {
@@ -137,7 +137,7 @@ export const columns: Column[] = [
     icon: 'wifi_tethering',
     type: 'chart',
     accessor: node => node.history || [],
-    show: value => `${value} ms`,
+    show: value => timeAgo(value as any, true, 5000),
   },
   {
     name: 'Uptime',
