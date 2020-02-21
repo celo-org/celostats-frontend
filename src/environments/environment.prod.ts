22967f815ab8f083a80b113e453e3393d87cf3ad
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-02-21 14:52:25 +0100

    Create a script that overwrite the environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
+import { DOCKER_ENV } from './docker-env'
+
 export const environment = {
   production: true,
-  ethstatsService: 'wss://baklava-ethstats.celo-testnet.org',
-  blockscoutUrl: 'https://baklava-blockscout.celo-testnet.org',
-  graphqlBlockscoutUrl: 'https://baklava-blockscout.celo-testnet.org/graphiql',
+  ethstatsService: DOCKER_ENV.ethstatsService || 'wss://baklava-ethstats.celo-testnet.org',
+  blockscoutUrl: DOCKER_ENV.blockscoutUrl || 'https://baklava-blockscout.celo-testnet.org',
+  graphqlBlockscoutUrl: DOCKER_ENV.graphqlBlockscoutUrl || 'https://baklava-blockscout.celo-testnet.org/graphiql',
 }
