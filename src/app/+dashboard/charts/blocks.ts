450aeb67278aba5e6bbabe64188bc5787deb8abf
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-02-26 16:49:07 +0100

    Add icons in the top data

--- src/app/+dashboard/charts/blocks.ts
+++ src/app/+dashboard/charts/blocks.ts
@@ -15,7 +15,7 @@ export interface Context {
 }
 
 interface InfoBlockBase<T extends infoType, V, R = V> {
-  icon: string
+  icon?: string
   title: string
   type: T
   accessor: (context: Context) => V
@@ -35,14 +35,13 @@ export const blocks: InfoBlock[][] = [
     {
       type: 'big',
       title: 'Block',
-      icon: 'archive',
+      icon: 'block',
       accessor: ({block}) => '#' + formatNumber(block.number, 0),
       color: () => 'info',
     },
     {
       type: 'chart',
       title: 'Block Time',
-      icon: 'av_timer',
       accessor: ({charts}) => (charts?.blocktime ?? []),
       show: (data, {charts, block: {number: block}}) => data
         .map((value, i) => ({
@@ -58,7 +57,6 @@ export const blocks: InfoBlock[][] = [
     {
       type: 'chart',
       title: 'Propagation Time',
-      icon: 'wifi_tethering',
       accessor: ({charts}) => (charts?.propagation?.histogram ?? []),
       show: data => data
         .map(({x, dx, y: value, cumpercent}, i, {length}) => ({
@@ -75,14 +73,13 @@ export const blocks: InfoBlock[][] = [
     {
       type: 'big',
       title: 'Blocks Until Epoch/\nEpoch Size',
-      icon: 'archive',
+      icon: 'blocks',
       accessor: ({block}) => `${block.blockRemain}/${block.epochSize}`,
       color: (value, {block}) => colorRange((block?.epochSize - +value) / block?.epochSize, [0, 0.5, 0.8]),
     },
     {
       type: 'medium',
       title: 'Last Block',
-      icon: 'hourglass_empty',
       accessor: ({block, time}) => Math.round((time - +block?.received) / 1000),
       show: value => `${value}s ago`,
       color: value => colorRange(+value, [4, 10, 30, 60, 120, 300]),
@@ -90,7 +87,6 @@ export const blocks: InfoBlock[][] = [
     {
       type: 'medium',
       title: 'Avg Block Time',
-      icon: 'timer',
       accessor: ({charts}) => +charts?.avgBlocktime,
       show: value => `${(+value).toFixed(2)}s`,
       color: value => colorRange(+value, [, 10, 30, 60, 600]),
@@ -100,7 +96,7 @@ export const blocks: InfoBlock[][] = [
     {
       type: 'big',
       title: 'Active Nodes',
-      icon: 'remove_from_queue',
+      icon: 'triangle',
       accessor: ({nodes}) => nodes.filter(node => node.stats?.active).length,
       show: (value, {nodes}) => `${value}/${nodes.length}`,
       color: (value, {nodes}) => colorRange((nodes.length - +value) / nodes.length, [, 0.1, 0.2, 0.5, , ]),
@@ -108,7 +104,6 @@ export const blocks: InfoBlock[][] = [
     {
       type: 'small',
       title: 'Gas Price',
-      icon: 'money',
       accessor: ({nodes}) => nodes[0]?.stats?.gasPrice / 10 ** 9,
       needsUpdate: (newValue, oldValue) => newValue !== 0 || !oldValue,
       show: value => `${value} gwei`,
@@ -117,7 +112,6 @@ export const blocks: InfoBlock[][] = [
     {
       type: 'small',
       title: 'Gas Limit',
-      icon: 'money',
       accessor: ({block}) => block.gasLimit,
       show: value => `${value} gas`,
       color: () => 'info',
@@ -125,7 +119,6 @@ export const blocks: InfoBlock[][] = [
     {
       type: 'chart',
       title: 'Gas Spending',
-      icon: 'attach_money',
       accessor: ({charts, block: {number: block}}) => (charts?.gasSpending ?? [])
         .map((value, i, {length}) => ({
           value,
@@ -141,15 +134,14 @@ export const blocks: InfoBlock[][] = [
   [
     {
       type: 'big',
+      icon: 'wide',
       title: 'Elected/Registered \nValidators',
-      icon: 'check_circle_outside',
       accessor: ({block}) => `${block.validators?.elected ?? 0}/${block.validators?.registered ?? 0}`,
       color: value => value ? 'ok' : 'no',
     },
     {
       type: 'block-proposers',
       title: 'Recent Block Proposers',
-      icon: 'done_all',
       accessor: ({charts}) => (charts?.miners ?? []),
       needsUpdate: (a, b) => JSON.stringify(a) !== JSON.stringify(b),
       color: () => 'info',
--- src/app/components/info/info.module.ts
+++ src/app/components/info/info.module.ts
@@ -6,8 +6,9 @@ import { ChartModule } from '../chart'
 import { BlockProposersModule } from '../block-proposers'
 
 import { InfoComponent } from './info.component'
+import { InfoIconComponent } from './info-icon.component'
 
-const declarations = [InfoComponent]
+const declarations = [InfoComponent, InfoIconComponent]
 const modules = [MatIconModule, ChartModule, BlockProposersModule]
 const providers = []
 
