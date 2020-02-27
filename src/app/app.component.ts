ba942df34b782da9fa57c3ab8277a0909333dce2
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-02-27 13:06:31 +0100

    Add links of external Celo tools

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core'
 
+import { environment } from 'src/environments/environment'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  submenu = environment.submenu
 }
--- src/environments/docker-env.ts
+++ src/environments/docker-env.ts
@@ -2,4 +2,6 @@ export const DOCKER_ENV = {
   ethstatsService: '%%DOCKER_ENV%%ETHSTATS_SERVICE%%',
   blockscoutUrl: '%%DOCKER_ENV%%BLOCKSCOUT_URL%%',
   graphqlBlockscoutUrl: '%%DOCKER_ENV%%GRAPHQL_BLOCKSCOUT_URL%%',
+  submenuExplorer: '%%DOCKER_ENV%%SUBMENU_EXPLORER%%',
+  submenuBlockscout: '%%DOCKER_ENV%%SUBMENU_BLOCKSCOUT%%',
 }
