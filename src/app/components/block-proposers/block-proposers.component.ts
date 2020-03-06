b6029b409f7823ee0ddfc845ea1feec63db699ca
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-03-06 15:47:02 +0100

    Add the loading effect in all the components of top data

--- src/app/components/block-proposers/block-proposers.component.ts
+++ src/app/components/block-proposers/block-proposers.component.ts
@@ -1,17 +1,31 @@
 import { Component, Input, OnChanges, SimpleChanges, ChangeDetectionStrategy } from '@angular/core'
-import { ReplaySubject } from 'rxjs'
+import { BehaviorSubject } from 'rxjs'
 import { Miner } from '@celo/celostats-server'
+
+import { animations } from 'src/app/shared'
 
 @Component({
   selector: 'component-block-proposers',
   templateUrl: './block-proposers.component.html',
   styleUrls: ['./block-proposers.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [animations.loadingAnimation],
 })
 export class BlockProposersComponent implements OnChanges {
   @Input() data: Miner[]
 
-  data$ = new ReplaySubject<(Miner & {blocks: boolean[]})[]>()
+  maxNumberOfRows = 12
+  maxNumberOfBlocks = 36
+  data$ = new BehaviorSubject<(Miner & {blocks: boolean[]})[]>(
+    new Array(this.maxNumberOfRows)
+      .fill({
+        loading: true,
+        number: '000000',
+        miner: '0'.repeat(40),
+        blocks: new Array(this.maxNumberOfBlocks).fill(false),
+      }) as any,
+  )
+  private minIndexNumber // Used to keep the same index to be able to perform the initial animation
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes.data) {
@@ -19,21 +33,28 @@ export class BlockProposersComponent implements OnChanges {
         return
       }
       const data: Miner[] = changes.data.currentValue
+      if (!this.minIndexNumber) {
+        this.minIndexNumber = data.reduce((acc, {number: n}) => Math.max(acc, n), 0)
+      }
       const cleanData = data
         .map((mined, i, list) => ({
           ...mined,
           miner: mined.miner.replace('0x', ''),
           blocks: list
-            .slice(0, 36)
+            .slice(0, this.maxNumberOfBlocks)
             .map(({miner}) => miner === mined.miner),
+          track: this.maxNumberOfRows - (mined.number - this.minIndexNumber + this.maxNumberOfRows),
         }))
-        .slice(0, 12)
+        .slice(0, this.maxNumberOfRows)
       this.data$.next(cleanData)
     }
   }
 
-  trackRow(i: number, block: Miner): string {
-    return String(block.number)
+  trackRow(i: number, block: Miner & {loading?: boolean, track: number}): string {
+    if (block.loading) {
+      return String(i)
+    }
+    return String(block.track)
   }
 
   trackIndex(index: number): string {
