b31ad82f5ef6904b4093014cf218f21fa36f9ef5
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-03-06 15:42:25 +0100

    Improve loading animation

--- src/app/shared/animations/loading.ts
+++ src/app/shared/animations/loading.ts
@@ -1,22 +1,23 @@
 import { trigger, state, style, group, query, transition, animate, AnimationTriggerMetadata } from '@angular/animations'
 
-const loadingAnimationTime = (delay: number = 0, time: number = 600) => `${time - delay}ms ${delay}ms ease`
+const loadingAnimationTime = (delay: number = 0, time: number = 800) => `${time - (time * delay)}ms ${time * delay}ms ease`
 
 export const loadingAnimation: AnimationTriggerMetadata = trigger('loadingAnimation', [
   state('*', style({
     background: 'currentColor',
-    opacity: 0.3,
+    opacity: 0.2,
     animation: 'loading-animation 1s ease-in-out infinite alternate',
   })),
   state('false', style({
     background: 'transparent',
     opacity: 1,
   })),
+  transition(':enter, :leave', animate(0)),
   transition('* => false', group([
     animate(loadingAnimationTime()),
-    query('span', [
+    query('[loadingAnimationText]', [
       style({opacity: 0}),
-      animate(loadingAnimationTime(200), style({opacity: 1})),
+      animate(loadingAnimationTime(0.4), style({opacity: 1})),
     ]),
   ])),
 ])
